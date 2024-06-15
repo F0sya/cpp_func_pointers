2ccd90e1851519060c9acb3835282761c220da64
F0sya <nikitachmyria@gmail.com>
2024-06-15 15:58:21 +0300

    First practice exercise is done.

--- cpp_func_pointers/main.cpp
+++ cpp_func_pointers/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
+template<typename T>
+T* createArray(int size) {
+	T* arr = new T[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 20;
+	}
+	return arr;
+}
+template<typename T>
+void printArray(T* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << *(arr + i) << " ";
+	}
+	cout << endl;
+}
+template<typename T>
+void sum_and_multiply(T* arr, int size, int* sum, int*multiply) {
+	for (int i = 0; i < size; i++) {
+		*sum += *(arr+i);
+		*multiply *= *(arr + i);
+	}
+}
+
 int main() {
+	srand(time(0));
+	//creating array
+	int* size = new int;
+	cout << "Enter a size of array:"; cin >> *size;
+	int* arr = createArray<int>(*size);
+	cout << "Array:" << endl; printArray(arr, *size);
+	
+	//sum and multiply
+	int sum = 0, multiply = 1;
+	sum_and_multiply<int>(arr, *size,&sum,&multiply);
+	cout << endl << "Summary: " << sum << endl << "Multiply: " << multiply << endl;
+	
 	return 0;
 }
